da13bf0f9548b3796fe1ae9f02c798317c4c9afe
Dasha <dariagaidabura@gmail.com>
2021-11-09 00:30:32 +0200

    lesson 5

---
+++ lesson5.js
@@ -0,0 +1,14 @@
+function doSubmit() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  document.getElementById("username_block").innerHTML = "username: " + username;
+  document.getElementById("password_block").innerHTML = "password: " + password;
+
+  setDataToLS(username, password);
+};
+
+function setDataToLS(username, password) {
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
+}
