4d5545fcc16be14a5ab41aa9198d6d2ef082da5c
Dasha <dariagaidabura@gmail.com>
2021-11-10 18:53:59 +0200

    lesson 5 fixed

--- lesson5.js
+++ lesson5.js
@@ -1,6 +1,6 @@
-function doSubmit() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+ butt.onclick = function doSubmit() {
+ let username = document.getElementById("username").value;
+ let password = document.getElementById("password").value;
 
   document.getElementById("username_block").innerHTML = "username: " + username;
   document.getElementById("password_block").innerHTML = "password: " + password;
@@ -12,3 +12,4 @@ function setDataToLS(username, password) {
   localStorage.setItem("username", username);
   localStorage.setItem("password", password);
 }
+
